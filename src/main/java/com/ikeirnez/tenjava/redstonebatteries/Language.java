9a30fa20984833eaf0ba0c3b683efa3d378e1672
Keir Nellyer <keir@nellyer.co.uk>
2014-07-12 10:53:17 +0100

    Added language

---
+++ src/main/java/com/ikeirnez/tenjava/redstonebatteries/Language.java
@@ -0,0 +1,45 @@
+package com.ikeirnez.tenjava.redstonebatteries;
+
+import org.bukkit.ChatColor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by iKeirNez on 12/07/2014.
+ */
+public class Language {
+
+    private Language(){}
+
+    private static Properties properties = new Properties();
+
+    static {
+        Language.class.getClassLoader().getResourceAsStream("language_en.properties");
+    }
+
+    public static void load(InputStream inputStream){
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String get(String key, Object... args){
+        if (getProperties().containsKey(key)){
+            return ChatColor.translateAlternateColorCodes('&', String.format(getRaw(key), args));
+        } else {
+            return key;
+        }
+    }
+
+    public String getRaw(String key){
+        return getProperties().getProperty(key);
+    }
+
+    public static Properties getProperties() {
+        return properties;
+    }
+}
