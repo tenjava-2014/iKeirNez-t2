a181a2a634e7201522170d8cdb07aa4db5e16232
Keir Nellyer <keir@nellyer.co.uk>
2014-07-12 10:14:12 +0100

    Initial Commit

---
+++ src/main/java/com/ikeirnez/tenjava/redstonebatteries/RedstoneBatteries.java
@@ -0,0 +1,20 @@
+package com.ikeirnez.tenjava.redstonebatteries;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main plugin class.
+ */
+public class RedstoneBatteries extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
