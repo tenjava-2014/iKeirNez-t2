9aac42f201ca456c5eef5bcb508984051cd6245f
Keir Nellyer <keir@nellyer.co.uk>
2014-07-12 17:44:35 +0100

    Progress update

--- src/main/java/com/ikeirnez/tenjava/redstonebatteries/structures/Battery.java
+++ src/main/java/com/ikeirnez/tenjava/redstonebatteries/structures/Battery.java
@@ -1,6 +1,12 @@
 package com.ikeirnez.tenjava.redstonebatteries.structures;
 
 import com.ikeirnez.tenjava.redstonebatteries.utilities.Cuboid;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by iKeirNez on 12/07/2014.
@@ -8,8 +14,15 @@ import com.ikeirnez.tenjava.redstonebatteries.utilities.Cuboid;
 public class Battery {
 
     private static final int BATTERY_SIZE_X = 3, BATTERY_SIZE_Z = 4, BATTERY_MIN_SIZE_Y = 5;
+    private static final Material INPUT_BLOCK = Material.ENDER_STONE, OUTPUT_BLOCK = Material.LAPIS_BLOCK, CHARGED_NOTIFIER_BLOCK = Material.PISTON_STICKY_BASE;
 
     private final Cuboid cuboid;
+    private int size;
+    private int maxCharge = 0;
+    private int charge = 0;
+
+    private final List<Location> glassBlocksLocations = new ArrayList<>(), snowBlocksLocations = new ArrayList<>();
+    private Location inputBlockLocation, outputBlockLocation, chargedNotifierBlock;
 
     // 8 snow pile = 1 block
 
@@ -18,6 +31,38 @@ public class Battery {
 
         if (getCuboid().getLengthX() != BATTERY_SIZE_X || getCuboid().getLengthZ() != BATTERY_SIZE_Z || getCuboid().getLengthY() < BATTERY_MIN_SIZE_Y){
             throw new IllegalArgumentException("Cuboid does not match dimensions of a Battery.");
+        }
+
+        size = getCuboid().getLengthY() - 4;
+
+        Location centre = getCuboid().getCentre();
+
+        // clean this up sometime
+        for (Block block : cuboid.getBlocks()){
+            Location location = block.getLocation();
+            Material type = block.getType();
+
+            if (type == Material.STAINED_GLASS){
+                if ((location.getX() == centre.getX() + 1 || location.getX() == centre.getX() - 1) && (location.getZ() == centre.getX() + 1 || location.getZ() == centre.getZ() - 1)){
+                    glassBlocksLocations.add(location);
+                }
+            } else if (type == Material.SNOW || type == Material.SNOW_BLOCK){
+                if (location.getX() == centre.getX() && location.getZ() == centre.getZ()){
+                    snowBlocksLocations.add(location);
+                }
+            } else if (type == INPUT_BLOCK){
+                if (location.getX() == centre.getX() && location.getZ() == centre.getZ()){
+                    inputBlockLocation = location;
+                }
+            } else if (type == OUTPUT_BLOCK){
+                if (location.getX() == centre.getX() && location.getZ() == centre.getZ()){
+                    outputBlockLocation = location;
+                }
+            } else if (type == CHARGED_NOTIFIER_BLOCK){
+                if ((location.getX() == centre.getX() + 2 || location.getX() == centre.getX() - 2) && (location.getZ() == centre.getZ() + 2 || location.getZ() == centre.getZ() - 2)){
+                    chargedNotifierBlock = location;
+                }
+            }
         }
     }
 
--- src/main/java/com/ikeirnez/tenjava/redstonebatteries/utilities/Cuboid.java
+++ src/main/java/com/ikeirnez/tenjava/redstonebatteries/utilities/Cuboid.java
@@ -1,10 +1,14 @@
 package com.ikeirnez.tenjava.redstonebatteries.utilities;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,6 +19,7 @@ public class Cuboid implements ConfigurationSerializable, Cloneable {
     private final String worldName;
     private final int x1, y1, z1;
     private final int x2, y2, z2;
+    private final List<Location> locations = new ArrayList<>();
 
     public Cuboid(Location location1, Location location2){
         if (!location1.getWorld().equals(location2.getWorld())){
@@ -30,10 +35,16 @@ public class Cuboid implements ConfigurationSerializable, Cloneable {
         x2 = Math.max(location1.getBlockX(), location2.getBlockX());
         y2 = Math.max(location1.getBlockY(), location2.getBlockY());
         z2 = Math.max(location1.getBlockZ(), location2.getBlockZ());
+
+        cacheLocations();
     }
 
     public Cuboid(Map<String, Object> data){
-        worldName = (String) data.get("worldName");
+        this.worldName = (String) data.get("worldName");
+
+        if (getWorld() == null){
+            throw new IllegalArgumentException("World \"" + worldName + "\" doesn't exist.");
+        }
 
         this.x1 = (Integer) data.get("x1");
         this.y1 = (Integer) data.get("y1");
@@ -42,6 +53,24 @@ public class Cuboid implements ConfigurationSerializable, Cloneable {
         this.x2 = (Integer) data.get("x2");
         this.y2 = (Integer) data.get("y2");
         this.z2 = (Integer) data.get("z2");
+
+        cacheLocations();
+    }
+
+    private void cacheLocations(){
+        World world = getWorld();
+
+        for (int x = getLowerX(); x <= getUpperX(); x++){
+            for (int y = getLowerY(); y <= getUpperY(); y++){
+                for (int z = getLowerZ(); z <= getUpperZ(); z++){
+                    locations.add(new Location(world, x, y, z));
+                }
+            }
+        }
+    }
+
+    public World getWorld(){
+        return Bukkit.getWorld(worldName);
     }
 
     public boolean contains(Block block){
@@ -64,6 +93,54 @@ public class Cuboid implements ConfigurationSerializable, Cloneable {
     }
     public int getLengthZ(){
         return (z2 - z1) + 1;
+    }
+
+    public int getUpperX(){
+        return x2;
+    }
+
+    public int getUpperY(){
+        return y2;
+    }
+
+    public int getUpperZ(){
+        return z2;
+    }
+
+    public int getLowerX(){
+        return x1;
+    }
+
+    public int getLowerY(){
+        return y1;
+    }
+
+    public int getLowerZ(){
+        return z1;
+    }
+
+    public List<Location> getLocations() {
+        return locations;
+    }
+
+    public List<Block> getBlocks(){
+        List<Block> blocks = new ArrayList<>();
+
+        for (Location location : getLocations()){
+            blocks.add(location.getBlock());
+        }
+
+        return blocks;
+    }
+
+    public Location getCentre(){
+        int x = getUpperX() + 1;
+        int y = getUpperY() + 1;
+        int z = getUpperZ() + 1;
+
+        return new Location(getWorld(), getLowerX() + (x - getLowerX()) / 2.0,
+                            getLowerY() + (y - getLowerY()) / 2.0,
+                            getLowerZ() + (z - getLowerZ()) / 2.0);
     }
 
     @Override
